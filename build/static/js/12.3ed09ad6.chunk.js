7b01cfc0fc54d4210952c36494b1889905de88e9
Ivan <vanya1254@yandex.ru>
2024-12-13 17:28:53 +0300

    feat: init

---
+++ build/static/js/12.3ed09ad6.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[12],{495:function(s,e,i){"use strict";i.r(e);i(0);var t=i(5);e.default=()=>Object(t.jsx)("div",{children:"Home component works!"})}}]);
+//# sourceMappingURL=12.3ed09ad6.chunk.js.map
